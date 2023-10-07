fcdd0692119c4a4b68970fef834c4f4cda53f52c
Vibhinn Singhal <vibhinnsinghal_ec21a18_61@dtu.ac.in>
2023-10-07 10:48:57 +0530

    hooks

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,6 +1,7 @@
 function customRender(reactElement, container) {
     const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children
+
     for (const prop in reactElement.props) {
         if (prop === 'children') continue
         domElement.setAttribute(prop, reactElement.props[prop])
